0d744f46b82744eba390369cdb3537e5dfec37bd
megaxd274 <kazemirova.l@gmail.com>
2025-05-04 23:55:15 +0300

    feat: add part of process functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { stdin, stdout, argv, pid, kill } from "process";
-import { EOL } from "os";
+import { stdin, stdout, argv, pid, kill, chdir, cwd } from "process";
+import { EOL, homedir } from "os";
+import path from "path";
+import { opendir } from "fs/promises";
 
 const getUsername = () => {
   const template = "--username=";
@@ -11,15 +13,48 @@ const getUsername = () => {
   return "Who are you?";
 };
 
-stdout.write(`Welcome to the File Manager, ${getUsername()}!${EOL}`);
+const pwd = () => {
+  stdout.write(`You are currently in ${cwd()}${EOL}`);
+};
 
-process.on("SIGINT", () => {
-  stdout.write(`Thank you for using File Manager, ${getUsername()}, goodbye!`);
-  process.exit(0);
-});
+const up = () => {
+  chdir("..");
+};
+const cd = (path) => {
+  chdir(path);
+};
+const ls = async () => {
+  const result = [];
+  for await (const element of await opendir(cwd())) {
+    result.push({
+      Name: element.name,
+      Type: element.isDirectory() ? "directory" : "file",
+    });
+  }
+  console.table(result);
+};
+
+stdout.write(`Welcome to the File Manager, ${getUsername()}!${EOL}`);
+cd(homedir());
 
 stdin.on("data", (data) => {
   if (data.includes(".exit")) {
-    kill(pid, "SIGINT");
+    process.emit("SIGINT");
   }
+  if (data.includes("up")) {
+    up();
+    cd(data.toString().split(" ")[1]);
+  }
+  if (data.includes("cd")) {
+    console.log(path.join(data.toString().split(" ")[1]));
+    cd(data.toString().trim().split(" ")[1]);
+  }
+  if (data.includes("ls")) {
+    ls();
+  }
+  pwd();
+});
+process.once("SIGINT", () => {
+  stdout.write(`Thank you for using File Manager, ${getUsername()}, goodbye!`);
+  process.exit(0);
 });
