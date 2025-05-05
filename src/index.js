a6d96b8052a2efb1c45cb983e8f21750650d3993
megaxd274 <kazemirova.l@gmail.com>
2025-05-05 22:50:16 +0300

    feat: add basic fs operations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { stdin, stdout, argv, pid, kill, chdir, cwd } from "process";
 import { EOL, homedir } from "os";
-import path from "path";
-import { opendir } from "fs/promises";
+import { opendir, writeFile, stat, mkdir, rename, rm } from "fs/promises";
+import { createReadStream, createWriteStream } from "fs";
+import { basename, dirname, join } from "path";
+import { pipeline } from "stream/promises";
 
 const getUsername = () => {
   const template = "--username=";
@@ -13,8 +15,13 @@ const getUsername = () => {
   return "Who are you?";
 };
 
+const parseInput = (input) => {
+  const args = input.toString().trim().split(" ");
+  return args;
+};
+
 const pwd = () => {
-  stdout.write(`You are currently in ${cwd()}${EOL}`);
+  stdout.write(`${EOL}You are currently in ${cwd()}${EOL}`);
 };
 
 const up = () => {
@@ -53,23 +60,97 @@ const ls = async () => {
   );
 };
 
+const cat = (path) => {
+  return new Promise((resolve, reject) => {
+    const read = createReadStream(path);
+    read.pipe(stdout);
+    read.on("end", resolve);
+    read.on("error", () => console.log("Operation failed"));
+  });
+};
+
+const add = async (filename) => {
+  try {
+    await stat(filename);
+    console.log("Operation failed");
+  } catch (error) {
+    await writeFile(filename, "");
+  }
+};
+
+const myMkdir = async (dirname) => {
+  try {
+    await stat(dirname);
+    console.log("Operation failed");
+  } catch (error) {
+    await mkdir(dirname, {});
+  }
+};
+const rn = async (oldPath, newFileName) => {
+  try {
+    const newPath = join(dirname(oldPath), newFileName);
+    await rename(oldPath, newPath);
+  } catch (error) {
+    console.log("Operation failed");
+  }
+};
+const cp = async (originPath, destinationPath) => {
+  const input = createReadStream(originPath);
+  const dest = join(destinationPath, basename(originPath));
+  const output = createWriteStream(dest);
+  try {
+    await pipeline(input, output);
+  } catch (error) {
+    console.log("Operation failed");
+  }
+};
+
+const myRm = async (path) => {
+  try {
+    await rm(path);
+  } catch (error) {
+    console.log("Operation failed");
+  }
+};
+
 stdout.write(`Welcome to the File Manager, ${getUsername()}!${EOL}`);
 cd(homedir());
 
-stdin.on("data", (data) => {
-  if (data.includes(".exit")) {
+stdin.on("data", async (data) => {
+  data = parseInput(data);
+  if (data[0].includes(".exit")) {
     process.emit("SIGINT");
   }
-  if (data.includes("up")) {
+  if (data[0].includes("up")) {
     up();
-    cd(data.toString().split(" ")[1]);
   }
-  if (data.includes("cd")) {
-    console.log(path.join(data.toString().split(" ")[1]));
-    cd(data.toString().trim().split(" ")[1]);
+  if (data[0].includes("cd")) {
+    cd(data[1]);
   }
-  if (data.includes("ls")) {
-    ls();
+  if (data[0].includes("ls")) {
+    await ls();
+  }
+  if (data[0].includes("cat")) {
+    await cat(data[1]);
+  }
+  if (data[0].includes("add")) {
+    await add(data[1]);
+  }
+  if (data[0].includes("mkdir")) {
+    await myMkdir(data[1]);
+  }
+  if (data[0].includes("rn")) {
+    await rn(data[1], data[2]);
+  }
+  if (data[0].includes("cp")) {
+    await cp(data[1], data[2]);
+  }
+  if (data[0].includes("mv")) {
+    await cp(data[1], data[2]);
+    await myRm(data[1]);
+  }
+  if (data[0].includes("rm")) {
+    await myRm(data[1]);
   }
   pwd();
 });
